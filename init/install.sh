bc67ac98de66d5282aa0d44f6b8bd77e8108f0ab
daiki328 <dk08sel11@i.softbank.jp>
2022-07-22 13:58:24 +0900

    add

---
+++ init/install.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# git repo
+git clone https://github.com/daiki328/isucon-template.git ||
+echo "git clone failed";
+
+# alp
+sudo apt update &&
+sudo apt install unzip &&
+wget https://github.com/tkuchiki/alp/releases/download/v1.0.10/alp_linux_arm64.zip &&
+unzip alp_linux_arm64.zip &&
+sudo install ./alp /usr/local/bin &&
+rm ./alp_linux_arm64.zip &&
+rm ./alp ||
+echo "alp install failed";
+
+# apache bench
+sudo apt install apache2-utils ||
+echo "apache2-utils install failed";
+
+# mysql
+sudo apt install mysql-server mysql-client ||
+echo "mysql install failed";
+
+# dstat
+sudo apt install dstat ||
+echo "dstat install failed";
+
+# k6
+wget https://github.com/grafana/k6/releases/download/v0.39.0/k6-v0.39.0-linux-arm64.tar.gz &&
+tar -xf k6-v0.39.0-linux-arm64.tar.gz &&
+sudo install ./k6-v0.39.0-linux-arm64/k6 /usr/local/bin/ &&
+rm ./k6-v0.39.0-linux-arm64.tar.gz &&
+rm -r ./k6-v0.39.0-linux-arm64 ||
+echo "k6 install failed";
+
+# pt-query-digest
+sudo apt install percona-toolkit ||
+echo "percona-toolkit install failed";
+
+# query-digester
+git clone https://github.com/kazeburo/query-digester.git &&
+sudo install ./query-digester/query-digester /usr/local/bin/ &&
+rm -r ./query-digester ||
+echo "query-digester install failed";
